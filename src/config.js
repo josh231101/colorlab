71b63c1a0266b693451ac1f442cfcb3c785bdda2
Josue Arreola <josue@heroguest.com>
2023-11-27 12:51:11 -0600

    update route

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   // PROD
-  homeUrl: 'https://josue.ngrok.io',
+  homeUrl: 'https://josue.ngrok.io/api/admin',
   // homeUrl: 'http://127.0.0.1:3333/api/admin',
   defaultTimezone: 'America/Mexico_City',
   imagesUrl: 'https://torniweb.s3.amazonaws.com/'
